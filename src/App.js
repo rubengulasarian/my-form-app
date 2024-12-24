ed3d907093d5c21f49b4ce9c953f809a459e0ce9
Ruben <rubengulasarian@gmail.com>
2024-12-24 22:15:14 +0400

    Updated

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Header from './components/Header';
-
-import AuthForm from './components/auth';
+import AuthForm from './components/AuthForm';
 
 function App() {
     return (
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { AuthProvider } from './context/AuthСontext'; 
+import { AuthProvider } from './context/AuthContext';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
