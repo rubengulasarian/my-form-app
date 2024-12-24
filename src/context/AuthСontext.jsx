38a55f0dbf730212c3f36168ff141323509f60e3
Ruben <rubengulasarian@gmail.com>
2024-12-24 21:35:41 +0400

    Fixed Auth import and added markup

---
+++ src/context/AuthСontext.jsx
@@ -0,0 +1,56 @@
+import React, { createContext, useState, useEffect } from 'react';
+
+// Создаем контекст
+export const AuthContext = createContext(null);
+
+// Создаем провайдер контекста
+export const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
+      const [isAuth, setIsAuth] = useState(false);
+
+    useEffect(() => {
+      const storedUser = localStorage.getItem('user');
+      if(storedUser){
+         setUser(JSON.parse(storedUser));
+         setIsAuth(true)
+        }
+        }, [])
+
+
+    const register = (userData) => {
+        // Mock registration logic (we will replace this later)
+           localStorage.setItem('user', JSON.stringify(userData));
+          setUser(userData);
+          setIsAuth(true);
+    };
+
+    const login = (userData) => {
+          // Mock login logic (we will replace this later)
+           localStorage.setItem('user', JSON.stringify(userData));
+           setUser(userData);
+           setIsAuth(true);
+    };
+
+    const logout = () => {
+        // Mock logout logic (we will replace this later)
+         localStorage.removeItem('user');
+        setUser(null);
+        setIsAuth(false)
+    };
+
+    const contextValue = {
+        user,
+        isAuth,
+        register,
+        login,
+        logout,
+    };
+
+    return (
+        <AuthContext.Provider value={contextValue}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthContext;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { AuthProvider } from './context/Authсontext'; // Импорт AuthProvider
+import { AuthProvider } from './context/AuthСontext'; // Импорт AuthProvider
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
