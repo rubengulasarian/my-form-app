4eb1b2109e28dfe3103dee78bbbebaab25c5c607
Ruben <rubengulasarian@gmail.com>
2024-12-30 21:36:12 +0400

    Fixed Auth

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { AuthProvider } from './context/AuthContext'; // Обрати внимание на импорт
+import { AuthProvider } from './my_forms_app/src/context/AuthContext'; // Обрати внимание на импорт
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
